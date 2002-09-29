265fb98dd75719d207974048877e942e4a951902
tim <tim>
2002-09-29 21:02:02 +0000

    Fixed typo, mostly to check if my cvs connection is working

--- java/util/concurrent/Cancellable.java
+++ java/util/concurrent/Cancellable.java
@@ -5,21 +5,21 @@ package java.util.concurrent;
  * of asynchronous tasks.
  **/
 public interface Cancellable {
-  /*** 
-   * Cancel execution of this task if it ha not already completed.
+  /***
+   * Cancel execution of this task if it has not already completed.
    * @param mayInterruptIfRunning true if the execution of this task
    * may be interrupted if it is currently running.  Otherwise,
    * in-progress executions are allowed to complete.
-   * @return true unless the task has already 
+   * @return true unless the task has already
    * been cancelled or completed.
    **/
   public boolean cancel(boolean mayInterruptIfRunning);
 
-  /** 
+  /**
    * Return true if this task was cancelled before completion.
    **/
-  public boolean isCancelled();  
-  
+  public boolean isCancelled();
+
   /**
    * Return true if this task has completed,
    * either normally or via cancellation,

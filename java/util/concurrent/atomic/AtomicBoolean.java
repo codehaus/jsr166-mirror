a3613a8ca5f13173653b716d3ecfd40b941c7f78
jozart <jozart>
2002-10-13 01:47:37 +0000

    Indentation step 4.

--- java/util/concurrent/atomic/AtomicBoolean.java
+++ java/util/concurrent/atomic/AtomicBoolean.java
@@ -1,45 +1,45 @@
 package java.util.concurrent.atomic;
+
 /**
  * An AtomicBoolean maintains a boolean value that is updated atomically.
  **/
-
 public final class AtomicBoolean implements java.io.Serializable { 
 
-  /**
-   * Create a new AtomicRef with the given initial value;
-   **/
-  public AtomicBoolean(boolean initialValue) {
-    // for now;
-  }
+    /**
+     * Create a new AtomicRef with the given initial value;
+     **/
+    public AtomicBoolean(boolean initialValue) {
+        // for now;
+    }
   
-  /**
-   * Get the current value
-   **/
-  public boolean get() {
-    return false; // for now
-  }
+    /**
+     * Get the current value
+     **/
+    public boolean get() {
+        return false; // for now
+    }
   
-  /**
-   * Set to the given value
-   **/
-  public void set(boolean newValue) {
-    // for now
-  }
+    /**
+     * Set to the given value
+     **/
+    public void set(boolean newValue) {
+        // for now
+    }
 
-  /**
-   * Set to the give value and return the old value
-   **/
-  public boolean getAndSet(boolean newValue) {
-    return false; // for now
-  }
+    /**
+     * Set to the give value and return the old value
+     **/
+    public boolean getAndSet(boolean newValue) {
+        return false; // for now
+    }
   
-  /**
-   * Atomically set the value to the given updated value
-   * if the current value is equal to the expected value.
-   * @return true if successful. 
-   **/
-  public boolean attemptUpdate(boolean expect, boolean update) {
-    return false; // for now
-  }
+    /**
+     * Atomically set the value to the given updated value
+     * if the current value is equal to the expected value.
+     * @return true if successful.
+     **/
+    public boolean attemptUpdate(boolean expect, boolean update) {
+        return false; // for now
+    }
   
 }
--- java/util/concurrent/atomic/AtomicReference.java
+++ java/util/concurrent/atomic/AtomicReference.java
@@ -1,50 +1,51 @@
 package java.util.concurrent.atomic;
+
 /**
  * An AtomicReference maintains an object reference that is updated atomically.
  **/
-
 public class AtomicReference implements java.io.Serializable { 
-  private volatile Object value;
 
-  /**
-   * Create a new AtomicReference with the given initial value;
-   **/
-  public AtomicReference(Object initialValue) {
-    value = initialValue;
-  }
-  
-  /**
-   * Get the current value
-   **/
-  public final Object get() {
-    return value;
-  }
-  
-  /**
-   * Set to the given value
-   **/
-  public final void set(Object newValue) {
-    value = newValue;
-  }
-  
-  /**
-   * Atomically set the value to the given updated value 
-   * if the current value <tt>==</tt> the expected value.
-   * @return true if successful. 
-   **/
-  public final boolean attemptUpdate(Object expect, Object update) {
-    return false;
-  }
+    private volatile Object value;
 
-  /**
-   * Set to the given value and return the old value
-   **/
-  public final Object getAndSet(Object newValue) {
-    for (;;) {
-      Object x = get();
-      if (attemptUpdate(x, newValue))
-        return x;
+    /**
+     * Create a new AtomicReference with the given initial value;
+     **/
+    public AtomicReference(Object initialValue) {
+        value = initialValue;
     }
-  }
+  
+    /**
+     * Get the current value
+     **/
+    public final Object get() {
+        return value;
+    }
+  
+    /**
+     * Set to the given value
+     **/
+    public final void set(Object newValue) {
+        value = newValue;
+    }
+  
+    /**
+     * Atomically set the value to the given updated value
+     * if the current value <tt>==</tt> the expected value.
+     * @return true if successful.
+     **/
+    public final boolean attemptUpdate(Object expect, Object update) {
+        return false;
+    }
+
+    /**
+     * Set to the given value and return the old value
+     **/
+    public final Object getAndSet(Object newValue) {
+        while (true) {
+            Object x = get();
+            if (attemptUpdate(x, newValue))
+                return x;
+        }
+    }
   
 }

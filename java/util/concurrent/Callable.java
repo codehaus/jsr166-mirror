11c291369c4cda7839a47fc696a22700303b4f01
jozart <jozart>
2003-01-20 10:51:48 +0000

    Style tweaks.

--- java/util/concurrent/Callable.java
+++ java/util/concurrent/Callable.java
@@ -1,22 +1,22 @@
 package java.util.concurrent;
 
 /**
- * A Callable computes a result, or throws an exception if unable to
- * do so.  The <tt>Callable</tt> interface plays a role similar to
- * that of <tt>Runnable</tt>, except that it represents methods that
- * return results and (possibly) throw exceptions.  The
- * <tt>Callable</tt> interface may be implemented by any class with a
- * method computing a function that can be invoked in a "blind"
- * fashion by some other execution agent, for example a
- * java.util.concurrent.Executor.  The most common implementations of
- * Callable are inner classes that supply arguments operated on by the
- * function.
+ * Computes a result, or throws an exception if unable to do so.
+ * The <tt>Callable</tt> interface plays a role similar to that of
+ * {@link java.lang.Runnable Runnable}, except that it represents
+ * methods that return results, and may throw exceptions.
  *
- * @see     java.util.concurrent.Executor
- * @see     java.util.concurrent.Future
- * @see     java.util.concurrent.FutureTask
- **/
+ * REWRITE OR REMOVE This interface may be
+ * implemented by any class with a method computing a function that
+ * can be invoked in a "blind" fashion by some other execution agent,
+ * such as an <tt>Executor</tt>.  The most common implementations of
+ * <tt>Callable</tt> are inner classes that supply arguments operated
+ * on by the function. REWRITE OR REMOVE
+ *
+ * @see Executor
+ * @see Future
+ * @see FutureTask
+ */
 public interface Callable<V> {
     V call() throws Exception;
 }
-

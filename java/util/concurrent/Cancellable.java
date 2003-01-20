c81a80b7a18416231e1c6dfc77c7f392ff12d5c6
jozart <jozart>
2003-01-20 09:52:16 +0000

    Removed extraneous "public" from interface methods. Added tt markup to some identifiers.

--- java/util/concurrent/Cancellable.java
+++ java/util/concurrent/Cancellable.java
@@ -7,30 +7,31 @@ package java.util.concurrent;
  * @see FutureTask
  **/
 public interface Cancellable {
+
     /**
      * Cancel execution of this task if it has not already completed.
-     * If the task has not started when cancel() is called, the task
+     * If the task has not started when <tt>cancel</tt> is called, the task
      * will be cancelled.  If it has already started, then whether or
      * not the computation is cancelled depends on the value of the
-     * interruptIfRunning argument.
+     * <tt>interruptIfRunning</tt> argument.
      * @param interruptIfRunning true if the execution of the run method
      * computing this value should be interrupted. Otherwise,
      * in-progress executions are allowed to complete.
      * @return true unless the task has already been cancelled or
      * completed.
      **/
-    public boolean cancel(boolean mayInterruptIfRunning);
+    boolean cancel(boolean mayInterruptIfRunning);
 
     /**
-     * Return true if this task was cancelled before completion.
+     * @return true if this task was cancelled before completion.
      **/
-    public boolean isCancelled();
+    boolean isCancelled();
 
     /**
-     * Return true if this task has completed, either normally or via
+     * @return true if this task has completed, either normally or via
      * cancellation.
      **/
-    public boolean isDone();
+    boolean isDone();
 
 }
 

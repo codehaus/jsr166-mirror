592f341de32e4e826949baf5d6c7dbac61c8f10b
jozart <jozart>
2003-01-20 14:06:04 +0000

    Leverage exception chain.  Target exception is cause.

--- java/util/concurrent/ExecutionException.java
+++ java/util/concurrent/ExecutionException.java
@@ -2,70 +2,50 @@ package java.util.concurrent;
 
 /**
  * Thrown when attempting to retrieve the results of an asychronous
- * task that aborted, failing to produce a result.  
- * @@@brian Why not just use standard exception chaining?  
+ * task that aborted, failing to produce a result.
+ * THROWN BY WHO??  MENTION THAT THIS IS WRAPPER FOR REAL CAUSE
  */
 public class ExecutionException extends Exception {
 
     /**
-     * This field holds the target if the ExecutionException(Throwable
-     * target) constructor was used to instantiate the object
-     *
-     * @serial
-     * */
-    private Throwable target;
-    
-    /**
-     * Constructs an <code>ExecutionException</code> with
-     * <code>null</code> as the target exception.
+     * Constructs a <tt>ExecutionException</tt> with <tt>null</tt> as its
+     * detail message. The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause(Throwable) initCause}.
      */
-    protected ExecutionException() {
-        super((Throwable)null);  // Disallow initCause
+    protected ExecutionException() { }
+
+    /**
+     * Constructs a <tt>ExecutionException</tt> with the specified detail
+     * message. The cause is not initialized, and may subsequently be
+     * initialized by a call to {@link #initCause(Throwable) initCause}.
+     *
+     * @param message the detail message.
+     */
+    protected ExecutionException(String message) {
+        super(message);
     }
 
     /**
-     * Constructs a ExecutionException with a target exception.
+     * Constructs a <tt>ExecutionException</tt> with the specified detail
+     * message and cause.
      *
-     * @param target the target exception
+     * @param  message the detail message.
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).
      */
-    public ExecutionException(Throwable target) {
-        super((Throwable)null);  // Disallow initCause
-        this.target = target;
+    public ExecutionException(String message, Throwable cause) {
+        super(message, cause);
     }
 
     /**
-     * Constructs a ExecutionException with a target exception
-     * and a detail message.
+     * Constructs a <tt>ExecutionException</tt> with the specified cause and
+     * a detail message of <tt>(cause==null ? null : cause.toString())</tt>
+     * (which typically contains the class and detail message of <tt>cause</tt>).
      *
-     * @param target the target exception
-     * @param s      the detail message
+     * @param  cause the cause (which is saved for later retrieval by the
+     *         {@link #getCause()} method).
      */
-    public ExecutionException(Throwable target, String s) {
-        super(s, null);  // Disallow initCause
-        this.target = target;
-    }
-
-    /**
-     * Get the thrown target exception.
-     *
-     * <p>This method predates the general-purpose exception chaining
-     * facility.  The {@link Throwable#getCause()} method is now the
-     * preferred means of obtaining this information.
-     *
-     * @return the thrown target exception (cause of this exception).  
-     */
-    public Throwable getTargetException() {
-        return target;
-    }
-
-    /**
-     * Returns the the cause of this exception (the thrown target exception,
-     * which may be <tt>null</tt>).
-     *
-     * @return  the cause of this exception.
-     * @since   1.4
-     */
-    public Throwable getCause() {
-        return target;
+    public ExecutionException(Throwable cause) {
+        super(cause);
     }
 }

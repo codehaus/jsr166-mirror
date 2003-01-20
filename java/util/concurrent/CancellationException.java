96494a97735531b8c6480d60af10e9fa684999f4
jozart <jozart>
2003-01-20 11:02:12 +0000

    Conform to conventions.

--- java/util/concurrent/CancellationException.java
+++ java/util/concurrent/CancellationException.java
@@ -3,18 +3,21 @@ package java.util.concurrent;
 /**
  * Thrown when trying to retrieve the results of an asynchronous task
  * to indicate that the task was cancelled before its completion.
- **/
+ * THROWN BY WHAT??
+ */
 public class CancellationException extends IllegalStateException {  
+
     /**
-     * Constructs a CancellationException with no specified detail
-     * message.
-     **/
+     * Constructs a <tt>CancellationException</tt> with no detail message.
+     */
     public CancellationException() {}
 
     /**
-     * Constructs a CancellationException with specified detail
+     * Constructs a <tt>CancellationException</tt> with specified detail
      * message.
-     **/
+     *
+     * @param message the detail message pertaining to this exception.
+     */
     public CancellationException(String detail) {
         super(detail);
     }

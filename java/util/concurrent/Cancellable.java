16c12b7be09b79411817538191589be5a9142431
jozart <jozart>
2003-01-20 10:16:52 +0000

    More tweaks in accordance with style guide.

--- java/util/concurrent/Cancellable.java
+++ java/util/concurrent/Cancellable.java
@@ -2,10 +2,10 @@ package java.util.concurrent;
 
 /**
  * Cancellable objects provide methods to cancel the asynchronous
- * operations they represent, and query their status to determine if
+ * operations they represent, and to query their status to determine if
  * they have been cancelled.
  * @see FutureTask
- **/
+ */
 public interface Cancellable {
 
     /**
@@ -14,24 +14,27 @@ public interface Cancellable {
      * will be cancelled.  If it has already started, then whether or
      * not the computation is cancelled depends on the value of the
      * <tt>interruptIfRunning</tt> argument.
-     * @param interruptIfRunning true if the execution of the run method
-     * computing this value should be interrupted. Otherwise,
+     *
+     * @param interruptIfRunning <tt>true</tt> if execution of the <tt>run</tt>
+     * method computing this value should be interrupted. Otherwise,
      * in-progress executions are allowed to complete.
-     * @return true unless the task has already been cancelled or
+     * @return <tt>true</tt> unless the task has already been cancelled or
      * completed.
-     **/
+     */
     boolean cancel(boolean mayInterruptIfRunning);
 
     /**
-     * @return true if this task was cancelled before completion.
-     **/
+     * Returns <tt>true</tt> if this task was cancelled before completion.
+     *
+     * @return <tt>true</tt> if this task was cancelled before completion.
+     */
     boolean isCancelled();
 
     /**
-     * @return true if this task has completed, either normally or via
+     * Returns <tt>true</tt> if this task has completed, either normally or via
      * cancellation.
-     **/
+     *
+     * @return <tt>true</tt> if this task has completed normally or was cancelled.
+     */
     boolean isDone();
-
 }
-

7d4703897304e3b281532d3d2f1b7756fb83d7f5
dholmes <dholmes>
2003-01-29 23:02:23 +0000

    Made consistent with other exceptions etc.

--- java/util/concurrent/ExecutionException.java
+++ java/util/concurrent/ExecutionException.java
@@ -3,13 +3,18 @@ package java.util.concurrent;
 /**
  * Thrown when attempting to retrieve the results of an asychronous
  * task that aborted, failing to produce a result.
- * THROWN BY WHO??  MENTION THAT THIS IS WRAPPER FOR REAL CAUSE
+ * @fixme THROWN BY WHO??  MENTION THAT THIS IS WRAPPER FOR REAL CAUSE
+ *
+ * @since 1.5
+ * @spec JSR-166
+ * @revised $Date: 2003/01/29 23:02:23 $
+ * @editor $Author: dholmes $
  */
 public class ExecutionException extends Exception {
 
     /**
-     * Constructs a <tt>ExecutionException</tt> with <tt>null</tt> as its
-     * detail message. The cause is not initialized, and may subsequently be
+     * Constructs a <tt>ExecutionException</tt> with no detail message.
+     * The cause is not initialized, and may subsequently be
      * initialized by a call to {@link #initCause(Throwable) initCause}.
      */
     protected ExecutionException() { }
@@ -19,9 +24,9 @@ public class ExecutionException extends Exception {
      * message. The cause is not initialized, and may subsequently be
      * initialized by a call to {@link #initCause(Throwable) initCause}.
      *
-     * @param message the detail message.
+     * @param detail the detail message.
      */
-    protected ExecutionException(String message) {
+    protected ExecutionException(String detail) {
         super(message);
     }
 
@@ -29,18 +34,21 @@ public class ExecutionException extends Exception {
      * Constructs a <tt>ExecutionException</tt> with the specified detail
      * message and cause.
      *
-     * @param  message the detail message.
+     * @param  detail the detail message.
      * @param  cause the cause (which is saved for later retrieval by the
      *         {@link #getCause()} method).
      */
-    public ExecutionException(String message, Throwable cause) {
+    public ExecutionException(String detail, Throwable cause) {
         super(message, cause);
     }
 
     /**
-     * Constructs a <tt>ExecutionException</tt> with the specified cause and
-     * a detail message of <tt>(cause==null ? null : cause.toString())</tt>
-     * (which typically contains the class and detail message of <tt>cause</tt>).
+     * Constructs a <tt>ExecutionException</tt> with the specified cause. 
+     * The detail message is set to:
+     * <pre>
+     *  (cause == null ? null : cause.toString())</pre>
+     * (which typically contains the class and detail message of 
+     * <tt>cause</tt>).
      *
      * @param  cause the cause (which is saved for later retrieval by the
      *         {@link #getCause()} method).

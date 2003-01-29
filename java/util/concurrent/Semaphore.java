b3c82957b6f1977b017f4848ce9b9098364e5aaf
dholmes <dholmes>
2003-01-29 07:08:21 +0000

    Fixed doc inconsistency in Semaphore. Added acquire(n)/release(n) methods and all variants to FifoSemaphore.

--- java/util/concurrent/Semaphore.java
+++ java/util/concurrent/Semaphore.java
@@ -87,7 +87,7 @@ package java.util.concurrent;
  *
  * @since 1.5
  * @spec JSR-166
- * @revised $Date: 2003/01/29 06:06:24 $
+ * @revised $Date: 2003/01/29 07:08:21 $
  * @editor $Author: dholmes $
  *
  */
@@ -159,7 +159,8 @@ public class Semaphore {
     public void acquireUninterruptibly() {}
 
     /**
-     * Acquires a permit only if one is available at the time of invocation.
+     * Acquires a permit from this semaphore, only if one is available at the 
+     * time of invocation.
      * <p>Acquires a permit, if one is available and returns immediately,
      * with the value <tt>true</tt>,
      * reducing the number of available permits by one.
@@ -175,8 +176,8 @@ public class Semaphore {
     }
 
     /**
-     * Acquires a permit if one becomes available  within the given waiting
-     * time and the
+     * Acquires a permit from this semaphore, if one becomes available 
+     * within the given waiting time and the
      * current thread has not been {@link Thread#interrupt interrupted}.
      * <p>Acquires a permit, if one is available and returns immediately,
      * with the value <tt>true</tt>,

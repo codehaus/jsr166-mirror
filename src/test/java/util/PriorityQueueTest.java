7c1e55d8ca949438d89a36e14d87a250db2de71d
tim <tim>
2003-06-04 16:06:15 +0000

    Added test that uses PQ(Collection).

--- src/test/java/util/PriorityQueueTest.java
+++ src/test/java/util/PriorityQueueTest.java
@@ -7,39 +7,9 @@ import junit.framework.TestCase;
  */
 public class PriorityQueueTest extends TestCase {
 
-    private static final long SEED = 37L;
+    public void testNaturalOrder () {
 
-    private static final int N = 100000;
-
-    private final Random random = new Random(SEED);
-    private final List<Integer> unsorted = new ArrayList<Integer>(N);
-    {
-        for (int i = 0; i < N; ++i) {
-            unsorted.add(new Integer(random.nextInt(N)));
-        }
-    }
-
-
-    public void testSortedOrder () {
         PriorityQueue<Integer> pq = new PriorityQueue<Integer>();
-        List<Integer> expected = new ArrayList<Integer>(unsorted);
-        Collections.sort(expected);
-        doTest(pq, expected);
-    }
-
-    public void testReverseOrder () {
-        Comparator<Integer> rev = new Comparator<Integer>() {
-            public int compare (Integer a, Integer b) {
-                return b.compareTo(a);
-            }
-        };
-        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(N, rev);
-        List<Integer> expected = new ArrayList<Integer>(unsorted);
-        Collections.sort(expected, rev);
-        doTest(pq, expected);
-    }
-
-    private void doTest (PriorityQueue<Integer> pq, List<Integer> expected) {
 
         assertEquals("new pqueue should be empty", 0, pq.size());
 
@@ -49,16 +19,64 @@ public class PriorityQueueTest extends TestCase {
 
         assertEquals("new pqueue should have N elements", N, pq.size());
 
-        List<Integer> sorted = new ArrayList<Integer>(N);
-        for (Integer k; (k = pq.poll()) != null; ) {
-            sorted.add(k);
-        }
+        List<Integer> pqorder = new ArrayList<Integer>(N);
+        for (Integer k; (k = pq.poll()) != null; ) pqorder.add(k);
 
+        Collections.sort(expected);
         assertEquals("pqueue should return elements in sorted order",
                      expected,
-                     sorted);
-
-        //System.out.println(unsorted.subList(0, 10));
-        //System.out.println(sorted.subList(0, 10));
+                     pqorder);
     }
+
+    public void testComparatorOrder () {
+
+        Comparator<Integer> rev = new Comparator<Integer>() {
+            public int compare (Integer a, Integer b) {
+                return b.compareTo(a);
+            }
+        };
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(N, rev);
+        for (Iterator<Integer> it = unsorted.iterator(); it.hasNext(); ) {
+            pq.add(it.next());
+        }
+
+        List<Integer> pqorder = new ArrayList<Integer>(N);
+        for (Integer k; (k = pq.poll()) != null; ) pqorder.add(k);
+
+        Collections.sort(expected, rev);
+        assertEquals("pqueue should return elements in sorted order",
+                     expected,
+                     pqorder);
+    }
+
+    public void testConstructFromCollection () {
+
+        PriorityQueue<Integer> pq = new PriorityQueue<Integer>(unsorted);
+
+        assertEquals("new pqueue should have some element count as unsorted",
+                     unsorted.size(),
+                     pq.size());
+
+        List<Integer> pqorder = new ArrayList<Integer>(N);
+        for (Integer k; (k = pq.poll()) != null; ) pqorder.add(k);
+
+        Collections.sort(expected);
+        assertEquals("pqueue should return elements in sorted order",
+                     expected,
+                     pqorder);
+    }
+
+
+    private static final long SEED = 37L;
+    private static final int N = 100000;
+
+    private final Random random = new Random(SEED);
+    private final List<Integer> unsorted = new ArrayList<Integer>(N);
+    {
+        for (int i = 0; i < N; ++i) {
+            unsorted.add(new Integer(random.nextInt(N)));
+        }
+    }
+    private final List<Integer> expected = new ArrayList<Integer>(unsorted);
+
 }

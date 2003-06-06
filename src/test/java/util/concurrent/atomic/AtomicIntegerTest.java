d8afdfa8255a0a0b865ddc10ef6a458e5bc1fae8
tim <tim>
2003-06-06 18:34:44 +0000

    Fixed emulation jar exclusions, uncommented first atomics test

--- src/test/java/util/concurrent/atomic/AtomicIntegerTest.java
+++ src/test/java/util/concurrent/atomic/AtomicIntegerTest.java
@@ -11,11 +11,9 @@ public class AtomicIntegerTest extends TestCase {
         int initval = 37;
         AtomicInteger ai = new AtomicInteger(initval);
         int newval = 42;
-        if (false /* remove once this is implemented */) {
-            assertEquals("getAndSet should return initial value,",
-                         initval, ai.getAndSet(newval));
-            assertEquals("get should return new value,",
-                         newval, ai.get());
-        }
+        assertEquals("getAndSet should return initial value,",
+                     initval, ai.getAndSet(newval));
+        assertEquals("get should return new value,",
+                     newval, ai.get());
     }
 }

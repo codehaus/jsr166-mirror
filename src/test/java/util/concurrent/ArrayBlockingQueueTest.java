a75d1fba75bff5699c2ae0f90f371ec9b1ac712f
tim <tim>
2003-06-06 18:17:40 +0000

    Atomics working, so no longer getting IllegalMonitorStateException.

--- src/test/java/util/concurrent/ArrayBlockingQueueTest.java
+++ src/test/java/util/concurrent/ArrayBlockingQueueTest.java
@@ -11,8 +11,6 @@ import junit.framework.TestCase;
  */
 public class ArrayBlockingQueueTest extends TestCase {
 
-    private final boolean EMULATING = true;
-
     public void testCapacity () {
 
         ArrayBlockingQueue<Integer> q = new ArrayBlockingQueue<Integer>(2);
@@ -55,53 +53,40 @@ public class ArrayBlockingQueueTest extends TestCase {
     }
 
     public void testOffer () {
-        // With emulation, at least:
-        //
-        // java.lang.IllegalMonitorStateException
-        //      at java.util.concurrent.ReentrantLock.checkOwner(ReentrantLock.java:331)
-        //      at java.util.concurrent.ReentrantLock.unlock(ReentrantLock.java:646)
-        //      at java.util.concurrent.AbstractBlockingQueueFromQueue.offer(AbstractBlockingQueueFromQueue.java:131)
-        //      at java.util.concurrent.ArrayBlockingQueueTest$1.run(ArrayBlockingQueueTest.java:50)
-        //      at java.util.concurrent.ThreadPoolExecutor$Worker.runTask(ThreadPoolExecutor.java:442)
-        //      at java.util.concurrent.ThreadPoolExecutor$Worker.run(ThreadPoolExecutor.java:477)
-        //      at java.lang.Thread.run(Thread.java:594)
 
         final ArrayBlockingQueue<Integer> q = new ArrayBlockingQueue<Integer>(2);
 
         q.add(1);
         q.add(2);
 
-        if (!EMULATING) {
+        Executor executor = Executors.newFixedThreadPool(2);
 
-            Executor executor = Executors.newFixedThreadPool(2);
-
-            executor.execute(new Runnable() {
-                public void run() {
-                    assertFalse("offer should be rejected", q.offer(3));
-                    try {
-                        assertTrue("offer should be accepted", q.offer(3, 1, TimeUnit.MILLISECONDS));
-                    }
-                    catch (IllegalMonitorStateException e) {
-                        e.printStackTrace(System.err);
-                        fail("illegal monitor state");
-                    }
-                    catch (InterruptedException e) {
-                        fail("should not be interrupted");
-                    }
+        executor.execute(new Runnable() {
+            public void run() {
+                assertFalse("offer should be rejected", q.offer(3));
+                try {
+                    assertTrue("offer should be accepted", q.offer(3, 1000, TimeUnit.MILLISECONDS));
                 }
-            });
-
-            executor.execute(new Runnable() {
-                public void run() {
-                    try {
-                        Thread.sleep(2000);
-                        assertEquals("first item in queue should be 1", 1, q.take());
-                    }
-                    catch (InterruptedException e) {
-                        fail("should not be interrupted");
-                    }
+                catch (IllegalMonitorStateException e) {
+                    e.printStackTrace(System.err);
+                    fail("illegal monitor state");
                 }
-            });
-        }
+                catch (InterruptedException e) {
+                    fail("should not be interrupted");
+                }
+            }
+        });
+
+        executor.execute(new Runnable() {
+            public void run() {
+                try {
+                    Thread.sleep(2000);
+                    assertEquals("first item in queue should be 1", 1, q.take());
+                }
+                catch (InterruptedException e) {
+                    fail("should not be interrupted");
+                }
+            }
+        });
     }
 }

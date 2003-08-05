32a9d38156d13bc6c66bbbc3d0c1565e15f140a2
dholmes <dholmes>
2003-08-05 06:38:51 +0000

    Fixed tense of summary sentences (Adds rather than add etc)

--- src/main/java/util/concurrent/BlockingQueue.java
+++ src/main/java/util/concurrent/BlockingQueue.java
@@ -91,7 +91,7 @@ import java.util.Queue;
  *
  * @since 1.5
  * @spec JSR-166
- * @revised $Date: 2003/08/04 01:59:40 $
+ * @revised $Date: 2003/08/05 06:38:51 $
  * @editor $Author: dholmes $
  * @author Doug Lea
  */
@@ -116,7 +116,7 @@ public interface BlockingQueue<E> extends Queue<E> {
     public boolean offer(E o);
 
     /**
-     * Add the specified element to this queue, waiting if necessary up to the
+     * Adds the specified element to this queue, waiting if necessary up to the
      * specified wait time for space to become available.
      * @param o the element to add
      * @param timeout how long to wait before giving up, in units of
@@ -132,7 +132,7 @@ public interface BlockingQueue<E> extends Queue<E> {
         throws InterruptedException;
 
     /**
-     * Retrieve and remove the head of this queue, waiting
+     * Retrieves and removes the head of this queue, waiting
      * if necessary up to the specified wait time if no elements are
      * present on this queue.
      * @param timeout how long to wait before giving up, in units of
@@ -147,7 +147,7 @@ public interface BlockingQueue<E> extends Queue<E> {
         throws InterruptedException;
 
     /**
-     * Retrieve and remove the head of this queue, waiting
+     * Retrieves and removes the head of this queue, waiting
      * if no elements are present on this queue.
      * @return the head of this queue
      * @throws InterruptedException if interrupted while waiting.
@@ -156,7 +156,7 @@ public interface BlockingQueue<E> extends Queue<E> {
 
 
     /**
-     * Add the specified element to this queue, waiting if necessary for
+     * Adds the specified element to this queue, waiting if necessary for
      * space to become available.
      * @param o the element to add
      * @throws InterruptedException if interrupted while waiting.
@@ -166,7 +166,7 @@ public interface BlockingQueue<E> extends Queue<E> {
 
 
     /**
-     * Return the number of elements that this queue can ideally (in
+     * Returns the number of elements that this queue can ideally (in
      * the absence of memory or resource constraints) accept without
      * blocking, or <tt>Integer.MAX_VALUE</tt> if there is no
      * intrinsic limit.

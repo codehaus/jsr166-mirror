0b88cbbdc4b6c391904bc4986005c44866e09833
dholmes <dholmes>
2003-08-25 22:42:18 +0000

    As per Eamonn's comments:  - added a paragraph describing park/unpark in terms of a single permit
    Rewrote all the method docs in the common style used for all blocking
    methods, and to rephrase in terms of the permit.

--- src/main/java/util/concurrent/locks/LockSupport.java
+++ src/main/java/util/concurrent/locks/LockSupport.java
@@ -10,14 +10,22 @@ import sun.misc.Unsafe;
 
 
 /**
- * Basic thread blocking primitives useful for creating lock and
- * synchronization classes. The <tt>park</tt> and <tt>unpark</tt>
+ * Basic thread blocking primitives useful for creating locks and
+ * other synchronization classes. 
+ * <p>This class associates with each thread that uses it, a permit
+ * (in the sense of the {@link java.util.concurrent.Semaphore Semaphore}
+ * class). A call to <tt>park</tt> will return immediately if the permit
+ * is available - consuming it in the process - otherwise it may block.
+ * A call to <tt>unpark</tt> makes the permit available, if it was not already
+ * available.
+ * <p>The <tt>park</tt> and <tt>unpark</tt>
  * methods provide a means of blocking and unblocking threads that
  * eliminate the main problem that cause the deprecated methods
  * <tt>Thread.suspend</tt> and <tt>Thread.resume</tt> to be unusable
- * for such purposes: Lost races between one thread invoking
+ * for such purposes: races between one thread invoking
  * <tt>park</tt> and another thread trying to <tt>unpark</tt> it
- * preserve liveness.  Even so, these methods are designed to be used
+ * preserve liveness, due to the permit.  
+ * Even so, these methods are designed to be used
  * as tools for creating higher-level synchronization utilities, and
  * are not in themselves useful for most concurrency control
  * applications.
@@ -60,9 +68,11 @@ public class LockSupport {
     private static final Unsafe unsafe =  Unsafe.getUnsafe();
 
     /**
-     * Unblock the given thread blocked on <tt>park</tt>, or, if it is not
-     * blocked, cause its subsequent call to <tt>park</tt> not to block.
-     * @param thread the thread to unpark, or null, in which case
+     * Make available the permit for the given thread, if it was not already
+     * available.
+     * If the thread was blocked on <tt>park</tt> then it will unblock. 
+     * Otherwise, it's next call to <tt>park</tt> is guaranteed not to block.
+     * @param thread the thread to unpark, or <tt>null</tt>, in which case
      * this operation has no effect.
      */
     public static void unpark(Thread thread) {
@@ -71,44 +81,83 @@ public class LockSupport {
     }
 
     /**
-     * Block the current thread, returning when a balancing
-     * <tt>unpark</tt> occurs, or a balancing <tt>unpark</tt> has
-     * already occurred, or the thread is interrupted, or spuriously
-     * (i.e., returning for no "reason"). This method does
-     * <em>not</em> report which of these caused the method to
-     * return. Callers may determine, for example, the interrupt
-     * status of the thread upon return.
+     * Disables the current thread for thread scheduling purposes unless the
+     * permit is available.
+     * <p>If the permit is available then it is consumed and the call returns
+     * immediately; otherwise 
+     * the current thread becomes disabled for thread scheduling 
+     * purposes and lies dormant until one of three things happens:
+     * <ul>
+     * <li>Some other thread invokes <tt>unpark</tt> with the current thread 
+     * as the target; or
+     * <li>Some other thread {@link Thread#interrupt interrupts} the current
+     * thread; or
+     * <li>The call spuriously (that is, for no reason) returns.
+     * </ul>
+     * <p>This method does <em>not</em> report which of these caused the 
+     * method to return. Callers should re-check the conditions which caused 
+     * the thread to park in the first place. Callers may also determine, 
+     * for example, the interrupt status of the thread upon return.
      */
     public static void park() {
         unsafe.park(false, 0);
     }
 
     /**
-     * Block the current thread, returning when a balancing
-     * <tt>unpark</tt> occurs, or a balancing <tt>unpark</tt> has
-     * already occurred, or the thread is interrupted, or the given
-     * nanoseconds have elapsed, or spuriously (i.e., returning for no
-     * "reason"). This method does <em>not</em> report which of these
-     * caused the method to return. Callers may determine, for
-     * example, the interrupt status or elapsed time upon return.
-     * @param nanos the maximum number of nanoseconds to block
+     * Disables the current thread for thread scheduling purposes, for up to
+     * the specified waiting time, unless the permit is available.
+     * <p>If the permit is available then it is consumed and the call returns
+     * immediately; otherwise 
+     * the current thread becomes disabled for thread scheduling 
+     * purposes and lies dormant until one of four things happens:
+     * <ul>
+     * <li>Some other thread invokes <tt>unpark</tt> with the current thread 
+     * as the target; or
+     * <li>Some other thread {@link Thread#interrupt interrupts} the current
+     * thread; or
+     * <li>The specified waiting time elapses; or
+     * <li>The call spuriously (that is, for no reason) returns.
+     * </ul>
+     * <p>This method does <em>not</em> report which of these caused the 
+     * method to return. Callers should re-check the conditions which caused 
+     * the thread to park in the first place. Callers may also determine, 
+     * for example, the interrupt status of the thread, or the elapsed time
+     * upon return.
+     *
+     * @param nanos the maximum number of nanoseconds to wait
      */
     public static void parkNanos(long nanos) {
         unsafe.park(false, nanos);   
     }
 
     /**
-     * Block the current thread, returning when a balancing
-     * <tt>unpark</tt> occurs, or a balancing <tt>unpark</tt> has
-     * already occurred, or the thread is interrupted, or the given
-     * deadline has elapsed, or spuriously (i.e., returning for no
-     * "reason"). This method does <em>not</em> report which of these
-     * caused the method to return. Callers may determine, for
-     * example, the interrupt status or current time upon return.
-     * @param deadline the maximum time, in milliseconds from the Epoch
+     * Disables the current thread for thread scheduling purposes, until
+     * the specified deadline, unless the permit is available.
+     * <p>If the permit is available then it is consumed and the call returns
+     * immediately; otherwise 
+     * the current thread becomes disabled for thread scheduling 
+     * purposes and lies dormant until one of four things happens:
+     * <ul>
+     * <li>Some other thread invokes <tt>unpark</tt> with the current thread 
+     * as the target; or
+     * <li>Some other thread {@link Thread#interrupt interrupts} the current
+     * thread; or
+     * <li>The specified deadline passes; or
+     * <li>The call spuriously (that is, for no reason) returns.
+     * </ul>
+     * <p>This method does <em>not</em> report which of these caused the 
+     * method to return. Callers should re-check the conditions which caused 
+     * the thread to park in the first place. Callers may also determine, 
+     * for example, the interrupt status of the thread, or the current time
+     * upon return.
+     *
+     * @param deadline the absolute time, in milliseconds from the Epoch, to
+     * wait until
      */
     public static void parkUntil(long deadline) {
         unsafe.park(true, deadline);   
     }
 
 }
+
+

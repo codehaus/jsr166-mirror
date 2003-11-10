d7c6763230671ccb22516a690470c3a5c91c96ff
jsr166 <jsr166>
2003-11-10 07:43:33 +0000

    Un-M-q

--- src/main/java/lang/System.java
+++ src/main/java/lang/System.java
@@ -306,16 +306,18 @@ public final class System {
 
     /**
      * Returns the current value of the most precise available system
-     * timer, in nanoseconds.  <p>This method can only be used to
-     * measure elapsed time and is not related to any other notion of
-     * system or wall-clock time.  The value returned represents
-     * nanoseconds since some fixed but arbitrary time (perhaps in the
-     * future, so values may be negative).  This method provides
-     * nanosecond precision, but not necessarily nanosecond
-     * accuracy. No guarantees are made about how frequently values
-     * change. Differences in successive calls that span greater than
-     * approximately 292 years (2<sup>63</sup> nanoseconds) will not
-     * accurately compute elapsed time due to numerical overflow.
+     * timer, in nanoseconds.
+     *
+     * <p>This method can only be used to measure elapsed time and is
+     * not related to any other notion of system or wall-clock time.
+     * The value returned represents nanoseconds since some fixed but
+     * arbitrary time (perhaps in the future, so values may be
+     * negative).  This method provides nanosecond precision, but not
+     * necessarily nanosecond accuracy. No guarantees are made about
+     * how frequently values change. Differences in successive calls
+     * that span greater than approximately 292 years (2<sup>63</sup>
+     * nanoseconds) will not accurately compute elapsed time due to
+     * numerical overflow.
      *
      * <p> For example, to measure how long some code takes to execute:
      * <pre>

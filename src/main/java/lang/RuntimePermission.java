b95cf9a6203a81fdc122f2106d9bcd4fc7bd0359
jsr166 <jsr166>
2004-01-16 01:29:55 +0000

    Martin Buchholz: sync Sun copyrights

--- src/main/java/lang/RuntimePermission.java
+++ src/main/java/lang/RuntimePermission.java
@@ -1,7 +1,7 @@
 /*
  * @(#)RuntimePermission.java	1.48 03/08/23
  *
- * Copyright 2003 Sun Microsystems, Inc. All rights reserved.
+ * Copyright 2004 Sun Microsystems, Inc. All rights reserved.
  * SUN PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
  */
 

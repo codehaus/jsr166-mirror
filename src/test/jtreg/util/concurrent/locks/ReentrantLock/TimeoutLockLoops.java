39511d1a6221589742b8d217e29c9e3829d833ef
dl <dl>
2004-01-21 15:20:56 +0000

    Add warmup code

--- src/test/jtreg/util/concurrent/locks/ReentrantLock/TimeoutLockLoops.java
+++ src/test/jtreg/util/concurrent/locks/ReentrantLock/TimeoutLockLoops.java
@@ -18,19 +18,23 @@ public final class TimeoutLockLoops {
     static final LoopHelpers.SimpleRandom rng = new LoopHelpers.SimpleRandom();
     static boolean print = false;
     static final int ITERS = Integer.MAX_VALUE;
-    static final long TIMEOUT = 100; 
+    static final long TIMEOUT = 8; 
 
     public static void main(String[] args) throws Exception {
-        int maxThreads = 8;
+        int maxThreads = 100;
         if (args.length > 0) 
             maxThreads = Integer.parseInt(args[0]);
+
+        // Warm up to avoid transient hotspot problems
+        for (int i = 1; i < 4; ++i) 
+            new SimpleReentrantLockLoop(i).test();
 
         print = true;
 
         for (int i = 1; i <= maxThreads; i += (i+1) >>> 1) {
             System.out.print("Threads: " + i);
             new ReentrantLockLoop(i).test();
-            Thread.sleep(TIMEOUT);
+            Thread.sleep(10);
         }
         pool.shutdown();
     }
@@ -39,7 +43,7 @@ public final class TimeoutLockLoops {
         private int v = rng.next();
         private volatile boolean completed;
         private volatile int result = 17;
-        private final ReentrantLock lock = new ReentrantLock(true);
+        private final ReentrantLock lock = new ReentrantLock();
         private final LoopHelpers.BarrierTimer timer = new LoopHelpers.BarrierTimer();
         private final CyclicBarrier barrier;
         private final int nthreads;
@@ -49,11 +53,15 @@ public final class TimeoutLockLoops {
         }
 
         final void test() throws Exception {
-            for (int i = 0; i < nthreads; ++i) 
+            for (int i = 0; i < nthreads; ++i) {
+                lock.lock();
                 pool.execute(this);
+                lock.unlock();
+            }
             barrier.await();
             Thread.sleep(TIMEOUT);
-            lock.lock();
+            while (!lock.tryLock()); // Jam lock
+            //            lock.lock();
             barrier.await();
             if (print) {
                 long time = timer.getTime();
@@ -74,14 +82,17 @@ public final class TimeoutLockLoops {
                 int sum = v;
                 int x = 0;
                 int n = ITERS;
+                final ReentrantLock lock = this.lock;
                 do {
                     if (!lock.tryLock(TIMEOUT, TimeUnit.MILLISECONDS))
                         break;
-                    try {
-                        v = x = LoopHelpers.compute1(v);
-                    }
-                    finally {
-                        lock.unlock();
+                    else {
+                        try {
+                            v = x = LoopHelpers.compute1(v);
+                        }
+                        finally {
+                            lock.unlock();
+                        }
                     }
                     sum += LoopHelpers.compute2(x);
                 } while (n-- > 0);
@@ -90,10 +101,73 @@ public final class TimeoutLockLoops {
                 barrier.await();
                 result += sum;
             }
+            catch (Exception ex) { 
+                ex.printStackTrace();
+                return; 
+            }
+        }
+    }
+
+    static final class SimpleReentrantLockLoop implements Runnable {
+        static int iters = 100000;
+        private int v = rng.next();
+        private volatile int result = 17;
+        private final ReentrantLock lock = new ReentrantLock();
+        private final LoopHelpers.BarrierTimer timer = new LoopHelpers.BarrierTimer();
+        private final CyclicBarrier barrier;
+        private final int nthreads;
+        SimpleReentrantLockLoop(int nthreads) {
+            this.nthreads = nthreads;
+            barrier = new CyclicBarrier(nthreads+1, timer);
+        }
+
+        final void test() throws Exception {
+            for (int i = 0; i < nthreads; ++i) 
+                pool.execute(this);
+            barrier.await();
+            barrier.await();
+            if (print) {
+                long time = timer.getTime();
+                long tpi = time / ((long)iters * nthreads);
+                System.out.print("\t" + LoopHelpers.rightJustify(tpi) + " ns per lock");
+                double secs = (double)(time) / 1000000000.0;
+                System.out.println("\t " + secs + "s run time");
+            }
+
+            int r = result;
+            if (r == 0) // avoid overoptimization
+                System.out.println("useless result: " + r);
+        }
+
+        public final void run() {
+            final ReentrantLock lock = this.lock;
+            try {
+                barrier.await(); 
+                int sum = v;
+                int x = 0;
+                int n = iters;
+                while (n-- > 0) {
+                    lock.lock();
+                    if ((n & 255) == 0)
+                        v = x = LoopHelpers.compute2(LoopHelpers.compute1(v));
+                    else
+                        v = x += ~(v - n);
+                    lock.unlock();
+
+                    // Once in a while, do something more expensive
+                    if ((~n & 255) == 0) 
+                        sum += LoopHelpers.compute1(LoopHelpers.compute2(x));
+                    else
+                        sum += sum ^ x;
+                } 
+                barrier.await();
+                result += sum;
+            }
             catch (Exception ie) { 
                 return; 
             }
         }
     }
+
 
 }

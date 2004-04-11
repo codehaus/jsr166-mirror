cbc6f3f54d37859d9125a12a8de4bdfeb7cd21e0
jsr166 <jsr166>
2004-04-11 01:37:41 +0000

    Added ArrayBlockingQueue mode for BoundedBufferDemo, and updated comments with new timings.

--- src/javaone/jsr166/bbuf/BoundedBufferDemo.java
+++ src/javaone/jsr166/bbuf/BoundedBufferDemo.java
@@ -6,33 +6,34 @@ import java.util.concurrent.*;
 
 /**
  * Compares performance of BoundedBuffer implementation using
- * native Conditions with an implementation using dl.u.c CondVars.
+ * native Conditions with an implementation using dl.u.c CondVars
+ * and an implementation using j.u.c ArrayBlockingQueue.
  *
  * Sample results obtained on Pentium 4 @ 3.2 GHz -client (Win XP SP1)
  *
- * count=100       size=1     NATIVE: 52164.70    DL_U_C: 41299.12
- * count=1000      size=1     NATIVE: 17886.59    DL_U_C: 21110.29
- * count=10000     size=1     NATIVE: 17736.65    DL_U_C: 21178.19
+ * count=  100     size=   1   CONDVAR: 44492.27   CONDITION: 48841.38   ABQ: 30594.15
+ * count= 1000     size=   1   CONDVAR: 21418.45   CONDITION: 18795.19   ABQ: 18852.92
+ * count=10000     size=   1   CONDVAR: 21274.89   CONDITION: 18072.15   ABQ: 18195.83
  *
- * count=100       size=2     NATIVE: 11318.42    DL_U_C: 29474.30
- * count=1000      size=2     NATIVE:  9456.33    DL_U_C: 27046.71
- * count=10000     size=2     NATIVE:  8995.79    DL_U_C: 27220.45
+ * count=  100     size=   2   CONDVAR: 26484.28   CONDITION: 10297.97   ABQ:  8798.86
+ * count= 1000     size=   2   CONDVAR: 27090.81   CONDITION: 10202.68   ABQ:  4347.01
+ * count=10000     size=   2   CONDVAR: 26570.30   CONDITION:  9336.42   ABQ:  3521.01
  *
- * count=100       size=4     NATIVE:  7838.94    DL_U_C: 23670.77
- * count=1000      size=4     NATIVE:  5007.80    DL_U_C: 22594.43
- * count=10000     size=4     NATIVE:  4648.41    DL_U_C: 22383.00
+ * count=  100     size=   4   CONDVAR: 23004.82   CONDITION:  5862.64   ABQ:  3952.65
+ * count= 1000     size=   4   CONDVAR: 21680.27   CONDITION:  4696.97   ABQ:  1974.28
+ * count=10000     size=   4   CONDVAR: 22222.41   CONDITION:  4647.62   ABQ:  1888.04
  *
- * count=100       size=8     NATIVE:  7565.63    DL_U_C: 22412.73
- * count=1000      size=8     NATIVE:  2654.70    DL_U_C: 22254.11
- * count=10000     size=8     NATIVE:  2468.30    DL_U_C: 22111.69
+ * count=  100     size=   8   CONDVAR: 19886.62   CONDITION:  4967.40   ABQ:  2799.95
+ * count= 1000     size=   8   CONDVAR: 21742.35   CONDITION:  2532.28   ABQ:  1635.44
+ * count=10000     size=   8   CONDVAR: 21753.01   CONDITION:  2465.09   ABQ:  1470.60
  *
- * count=100       size=64    NATIVE:  4743.20    DL_U_C: 20079.90
- * count=1000      size=64    NATIVE:  2074.30    DL_U_C: 21592.09
- * count=10000     size=64    NATIVE:  1378.57    DL_U_C: 21692.48
+ * count=  100     size=  64   CONDVAR: 16366.01   CONDITION:  4921.32   ABQ:  2698.38
+ * count= 1000     size=  64   CONDVAR: 19637.40   CONDITION:  1632.69   ABQ:  1745.33
+ * count=10000     size=  64   CONDVAR: 21205.39   CONDITION:  1521.99   ABQ:  1690.17
  *
- * count=100       size=1024  NATIVE:  5377.27    DL_U_C: 17551.21
- * count=1000      size=1024  NATIVE:  1170.03    DL_U_C: 16591.20
- * count=10000     size=1024  NATIVE:  1369.83    DL_U_C: 21369.30
+ * count=  100     size=1024   CONDVAR: 13665.57   CONDITION:  2242.70   ABQ:  2708.56
+ * count= 1000     size=1024   CONDVAR: 14828.23   CONDITION:  1504.33   ABQ:  1859.53
+ * count=10000     size=1024   CONDVAR: 20851.61   CONDITION:  1396.44   ABQ:  1828.14
  */
 public final class BoundedBufferDemo {
 
@@ -40,7 +41,35 @@ public final class BoundedBufferDemo {
         new BoundedBufferDemo().run();
     }
 
-    enum Mode { NATIVE, DL_U_C }
+
+    abstract enum Mode {
+        CONDVAR {
+            BoundedBuffer<Integer> newBuffer(int size) {
+                return new CondVarBoundedBuffer<Integer>(size);
+            }
+        },
+        CONDITION {
+            BoundedBuffer<Integer> newBuffer(int size) {
+                return new ConditionBoundedBuffer<Integer>(size);
+            }
+        },
+        ABQ {
+            BoundedBuffer<Integer> newBuffer(final int size) {
+                return new BoundedBuffer<Integer>() {
+                    public void put(Integer element) throws InterruptedException {
+                        abq.put(element);
+                    }
+                    public Integer take() throws InterruptedException {
+                        return abq.take();
+                    }
+                    private final ArrayBlockingQueue<Integer> abq
+                        = new ArrayBlockingQueue<Integer>(size);
+                };
+            }
+        },
+    ;
+        abstract BoundedBuffer<Integer> newBuffer(int size);
+    }
 
     void run() {
         int[] counts = { 100, 1000, 10000 };
@@ -65,9 +94,7 @@ public final class BoundedBufferDemo {
 
     private long trial(final int count, int size, Mode mode) {
 
-        final BoundedBuffer<Integer> buf = (mode == Mode.NATIVE) ?
-                new ConditionBoundedBuffer<Integer>(size) :
-                new CondVarBoundedBuffer<Integer>(size);
+        final BoundedBuffer<Integer> buf = mode.newBuffer(size);
 
         ExecutorService executor = Executors.newFixedThreadPool(2);
         try {

d66beff0b8be6f0b1b0fe96dc03c78135468e066
jsr166 <jsr166>
2004-04-14 01:22:52 +0000

    Martin Buchholz: sync with Tiger

--- src/main/java/lang/Thread.java
+++ src/main/java/lang/Thread.java
@@ -12,6 +12,7 @@ import java.security.AccessControlContext;
 import java.util.Map;
 import java.util.HashMap;
 import java.util.Collections;
+import java.util.concurrent.locks.LockSupport;
 import sun.nio.ch.Interruptible;
 import sun.security.util.SecurityConstants;
 
@@ -316,8 +317,8 @@ class Thread implements Runnable {
 	this.target = target;
 	setPriority(priority);
         if (parent.inheritableThreadLocals != null)
-            this.inheritableThreadLocals = 
-                ThreadLocal.createInheritedMap(parent.inheritableThreadLocals);
+	    this.inheritableThreadLocals =
+		ThreadLocal.createInheritedMap(parent.inheritableThreadLocals);
         /* Stash the specified stack size in case the VM cares */
         this.stackSize = stackSize;
 
@@ -1427,6 +1428,119 @@ class Thread implements Runnable {
     public long getId() {
         return tid;
     }
+
+    /**
+     * A thread state.  A thread can be in one of the following states: 
+     * <ul>
+     * <li>{@link #NEW}<br>
+     *     A thread that has not yet started is in this state.
+     *     </li>
+     * <li>{@link #RUNNABLE}<br>
+     *     A thread executing in the Java virtual machine is in this state. 
+     *     </li>
+     * <li>{@link #BLOCKED}<br>
+     *     A thread that is blocked waiting for a monitor lock 
+     *     is in this state. 
+     *     </li>
+     * <li>{@link #WAITING}<br>
+     *     A thread that is waiting indefinitely for another thread to 
+     *     perform a particular action is in this state. 
+     *     </li>
+     * <li>{@link #TIMED_WAITING}<br>
+     *     A thread that is waiting for another thread to perform an action 
+     *     for up to a specified waiting time is in this state. 
+     *     </li>
+     * <li>{@link #TERMINATED}<br> 
+     *     A thread that has exited is in this state.
+     *     </li>
+     * </ul>
+     *
+     * <p>
+     * A thread can be in only one state at a given point in time. 
+     * These states are virtual machine states which do not reflect
+     * any operating system thread states.
+     * 
+     * @since   1.5
+     * @see Thread#getState
+     */
+    public enum State {
+        /**
+         * Thread state for a thread which has not yet started.
+         */
+        NEW,
+        
+        /**
+         * Thread state for a runnable thread.  A thread in the runnable
+         * state is executing in the Java virtual machine but it may
+         * be waiting for other resources from the operating system
+         * such as processor.
+         */
+        RUNNABLE,
+        
+        /**
+         * Thread state for a terminated thread.
+         * The thread has completed execution.
+         */
+        TERMINATED,
+
+        /**
+         * Thread state for a thread blocked waiting for a monitor lock.
+         * A thread in the blocked state is waiting for a monitor lock
+         * to enter a synchronized block/method or 
+         * reenter a synchronized block/method after calling
+         * {@link Object#wait() Object.wait}.
+         */
+        BLOCKED,
+    
+        /**
+         * Thread state for a waiting thread.
+         * A thread is in the waiting state due to calling one of the 
+         * following methods:
+         * <ul>
+         *   <li>{@link Object#wait() Object.wait} with no timeout</li>
+         *   <li>{@link Thread#join() Thread.join} with no timeout</li>
+         *   <li>{@link LockSupport#park() LockSupport.park}</li>
+         * </ul>
+         * 
+         * <p>A thread in the waiting state is waiting for another thread to
+         * perform a particular action.  
+         *
+         * For example, a thread that has called <tt>Object.wait()</tt>
+         * on an object is waiting for another thread to call 
+         * <tt>Object.notify()</tt> or <tt>Object.notifyAll()</tt> on 
+         * that object. A thread that has called <tt>Thread.join()</tt> 
+         * is waiting for a specified thread to terminate.
+         */
+        WAITING,
+        
+        /**
+         * Thread state for a waiting thread with a specified waiting time.
+         * A thread is in the timed waiting state due to calling one of 
+         * the following methods with a specified positive waiting time:
+         * <ul>
+         *   <li>{@link Thread#sleep Thread.sleep}</li>
+         *   <li>{@link Object#wait(long) Object.wait} with timeout</li>
+         *   <li>{@link Thread#join(long) Thread.join} with timeout</li>
+         *   <li>{@link LockSupport#parkNanos LockSupport.parkNanos}</li> 
+         *   <li>{@link LockSupport#parkUntil LockSupport.parkUntil}</li>
+         * </ul>
+         */
+        TIMED_WAITING;
+    }
+
+    /**
+     * Returns the state of this thread.
+     * This method is designed for use in monitoring of the system state,
+     * not for synchronization control.
+     * 
+     * @return this thread's state.
+     * @since 1.5
+     */
+    public State getState() {
+        // get current thread state
+        return sun.misc.VM.toThreadState(threadStatus);
+    }
+
     // Added in JSR-166
 
     /**

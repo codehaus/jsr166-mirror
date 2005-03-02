9a3fc9c258976a78cf415ce7f2ae0e8d256779d8
dl <dl>
2005-03-02 17:15:51 +0000

    Test for garbage retention with timeouts

--- src/test/jtreg/util/concurrent/BlockingQueue/PollMemoryLeak.java
+++ src/test/jtreg/util/concurrent/BlockingQueue/PollMemoryLeak.java
@@ -2,7 +2,7 @@
  * @test %I% %E%
  * @bug xxxxxx
  * @compile PollMemoryLeak.java
- * @run -Xmx32m main/timeout=3600 PollMemoryLeak
+ * @run -Xmx16m main/timeout=3600 PollMemoryLeak
  * @summary  Checks for OutOfMemoryError when an unbounded
  * number of aborted timed waits occur without a signal.
  */
@@ -13,9 +13,17 @@ import java.util.concurrent.*;
 
 public class PollMemoryLeak {
     public static void main(String[] args) throws InterruptedException {
-        BlockingQueue  bl = new LinkedBlockingQueue(2000);
-        while(true) {
-            bl.poll(1, TimeUnit.NANOSECONDS);
+        BlockingQueue b1 = new LinkedBlockingQueue(10);
+        BlockingQueue b2 = new ArrayBlockingQueue(10);
+        BlockingQueue b3 = new SynchronousQueue();
+        BlockingQueue b4 = new SynchronousQueue(true);
+        long start = System.currentTimeMillis();
+        long end = start + 2 * 60 * 1000; // 2 minutes
+        while(System.currentTimeMillis() < end) {
+            b1.poll(1, TimeUnit.NANOSECONDS);
+            b2.poll(1, TimeUnit.NANOSECONDS);
+            b3.poll(1, TimeUnit.NANOSECONDS);
+            b4.poll(1, TimeUnit.NANOSECONDS);
         }
     }
 }

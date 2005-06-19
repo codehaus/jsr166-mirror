2dc6ec88216012d26ec1cba06506d9477e02491e
dl <dl>
2005-06-19 23:01:20 +0000

    Clarify comment

--- src/main/java/util/concurrent/ConcurrentSkipListMap.java
+++ src/main/java/util/concurrent/ConcurrentSkipListMap.java
@@ -891,7 +891,8 @@ public class ConcurrentSkipListMap<K,V> extends AbstractMap<K,V>
 
     /**
      * Returns a random level for inserting a new node.
-     * Hardwired to k=1, p=0.5, max 31.
+     * Hardwired to k=1, p=0.5, max 31 (see
+     * Pugh's "Skip List Cookbook", sec 3.4).
      *
      * This uses the simplest of the generators described in George
      * Marsaglia's "Xorshift RNGs" paper.  This is not a high-quality

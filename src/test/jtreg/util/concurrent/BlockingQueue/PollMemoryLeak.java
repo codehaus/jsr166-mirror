8e57feec279d13374dceda9e49a6d8d8f16b6a8a
jsr166 <jsr166>
2005-07-18 02:16:20 +0000

    fix test failure; reduce timeout

--- src/test/jtreg/util/concurrent/BlockingQueue/PollMemoryLeak.java
+++ src/test/jtreg/util/concurrent/BlockingQueue/PollMemoryLeak.java
@@ -2,28 +2,25 @@
  * @test %I% %E%
  * @bug 6236036
  * @compile PollMemoryLeak.java
- * @run -Xmx16m main/timeout=3600 PollMemoryLeak
+ * @run main/othervm -Xmx8m PollMemoryLeak
  * @summary  Checks for OutOfMemoryError when an unbounded
  * number of aborted timed waits occur without a signal.
  */
 
-
 import java.util.concurrent.*;
-
 
 public class PollMemoryLeak {
     public static void main(String[] args) throws InterruptedException {
-        BlockingQueue b1 = new LinkedBlockingQueue(10);
-        BlockingQueue b2 = new ArrayBlockingQueue(10);
-        BlockingQueue b3 = new SynchronousQueue();
-        BlockingQueue b4 = new SynchronousQueue(true);
-        long start = System.currentTimeMillis();
-        long end = start + 2 * 60 * 1000; // 2 minutes
-        while(System.currentTimeMillis() < end) {
-            b1.poll(1, TimeUnit.NANOSECONDS);
-            b2.poll(1, TimeUnit.NANOSECONDS);
-            b3.poll(1, TimeUnit.NANOSECONDS);
-            b4.poll(1, TimeUnit.NANOSECONDS);
-        }
+	final BlockingQueue[] qs = {
+	    new LinkedBlockingQueue(10),
+	    new ArrayBlockingQueue(10),
+	    new SynchronousQueue(),
+	    new SynchronousQueue(true),
+	};
+        final long start = System.currentTimeMillis();
+        final long end = start + 10 * 1000;
+        while (System.currentTimeMillis() < end)
+	    for (BlockingQueue q : qs)
+		q.poll(1, TimeUnit.NANOSECONDS);
     }
 }

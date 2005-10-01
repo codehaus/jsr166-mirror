21718dddc4f3d955af1a757d9ff28acd8cc8122e
jsr166 <jsr166>
2005-10-01 22:07:26 +0000

    minor rewording

--- src/main/java/util/Stack.java
+++ src/main/java/util/Stack.java
@@ -8,22 +8,21 @@
 package java.util;
 
 /**
- * The <code>Stack</code> class represents a last-in-first-out 
- * (LIFO) stack of objects. It extends class <tt>Vector</tt> with five 
- * operations that allow a vector to be treated as a stack. The usual 
+ * The <code>Stack</code> class represents a last-in-first-out
+ * (LIFO) stack of objects. It extends class <tt>Vector</tt> with five
+ * operations that allow a vector to be treated as a stack. The usual
  * <tt>push</tt> and <tt>pop</tt> operations are provided, as well as a
- * method to <tt>peek</tt> at the top item on the stack, a method to test 
- * for whether the stack is <tt>empty</tt>, and a method to <tt>search</tt> 
+ * method to <tt>peek</tt> at the top item on the stack, a method to test
+ * for whether the stack is <tt>empty</tt>, and a method to <tt>search</tt>
  * the stack for an item and discover how far it is from the top.
  * <p>
- * When a stack is first created, it contains no items. 
+ * When a stack is first created, it contains no items.
  *
- * <p>A more complete and consistent set of LIFO stack operations are
- * available in {@link Deque} interface and its implementations, which
+ * <p>A more complete and consistent set of LIFO stack operations is
+ * provided by the {@link Deque} interface and its implementations, which
  * should be used in preference to this class.  For example:
- * <pre>
- *   Deque&lt;Integer&gt; stack = new ArrayDeque&lt;Integer&gt();
- * </pre>
+ * <pre>   {@code
+ *   Deque<Integer> stack = new ArrayDeque<Integer>();}</pre>
  *
  * @author  Jonathan Payne
  * @version %I%, %G%
@@ -38,7 +37,7 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Pushes an item onto the top of this stack. This has exactly 
+     * Pushes an item onto the top of this stack. This has exactly
      * the same effect as:
      * <blockquote><pre>
      * addElement(item)</pre></blockquote>
@@ -54,10 +53,10 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Removes the object at the top of this stack and returns that 
-     * object as the value of this function. 
+     * Removes the object at the top of this stack and returns that
+     * object as the value of this function.
      *
-     * @return     The object at the top of this stack (the last item 
+     * @return     The object at the top of this stack (the last item
      *             of the <tt>Vector</tt> object).
      * @exception  EmptyStackException  if this stack is empty.
      */
@@ -72,11 +71,11 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Looks at the object at the top of this stack without removing it 
-     * from the stack. 
+     * Looks at the object at the top of this stack without removing it
+     * from the stack.
      *
-     * @return     the object at the top of this stack (the last item 
-     *             of the <tt>Vector</tt> object). 
+     * @return     the object at the top of this stack (the last item
+     *             of the <tt>Vector</tt> object).
      * @exception  EmptyStackException  if this stack is empty.
      */
     public synchronized E peek() {
@@ -90,7 +89,7 @@ class Stack<E> extends Vector<E> {
     /**
      * Tests if this stack is empty.
      *
-     * @return  <code>true</code> if and only if this stack contains 
+     * @return  <code>true</code> if and only if this stack contains
      *          no items; <code>false</code> otherwise.
      */
     public boolean empty() {
@@ -98,16 +97,16 @@ class Stack<E> extends Vector<E> {
     }
 
     /**
-     * Returns the 1-based position where an object is on this stack. 
-     * If the object <tt>o</tt> occurs as an item in this stack, this 
-     * method returns the distance from the top of the stack of the 
-     * occurrence nearest the top of the stack; the topmost item on the 
-     * stack is considered to be at distance <tt>1</tt>. The <tt>equals</tt> 
-     * method is used to compare <tt>o</tt> to the 
+     * Returns the 1-based position where an object is on this stack.
+     * If the object <tt>o</tt> occurs as an item in this stack, this
+     * method returns the distance from the top of the stack of the
+     * occurrence nearest the top of the stack; the topmost item on the
+     * stack is considered to be at distance <tt>1</tt>. The <tt>equals</tt>
+     * method is used to compare <tt>o</tt> to the
      * items in this stack.
      *
      * @param   o   the desired object.
-     * @return  the 1-based position from the top of the stack where 
+     * @return  the 1-based position from the top of the stack where
      *          the object is located; the return value <code>-1</code>
      *          indicates that the object is not on the stack.
      */

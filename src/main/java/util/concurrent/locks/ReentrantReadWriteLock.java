049f6d83e6c0c46af2b04bd80d690388148b2d47
dl <dl>
2006-01-29 21:17:30 +0000

    Add writeLock.sHeldByCurrentThread() and getHoldCount()

--- src/main/java/util/concurrent/locks/ReentrantReadWriteLock.java
+++ src/main/java/util/concurrent/locks/ReentrantReadWriteLock.java
@@ -1043,6 +1043,30 @@ public class ReentrantReadWriteLock implements ReadWriteLock, java.io.Serializab
                                        "[Locked by thread " + o.getName() + "]");
         }
 
+        /**
+         * Queries if this write lock is held by the current thread.
+         * Identical in effect to {@link
+         * ReentrantReadWriteLock#isWriteLockedByCurrentThread}.
+         *
+         * @return <tt>true</tt> if current thread holds this lock and
+         * <tt>false</tt> otherwise.
+         */
+        public boolean isHeldByCurrentThread() {
+            return sync.isHeldExclusively();
+        }
+
+        /**
+         * Queries the number of holds on this write lock by the current
+         * thread.  Identical in effect to {@link
+         * ReentrantReadWriteLock#getWriteHoldCount}
+         *
+         * @return the number of holds on this lock by the current thread,
+         * or zero if this lock is not held by the current thread.
+         */
+        public int getHoldCount() {
+            return sync.getWriteHoldCount();
+        }
+
     }
 
     // Instrumentation and status

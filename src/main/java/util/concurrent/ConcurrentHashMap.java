932d61154095366200941d40159af1be2f4da4d6
jsr166 <jsr166>
2006-06-07 23:54:23 +0000

    6435782: (coll) Improve ConcurrentHashMap performance

--- src/main/java/util/concurrent/ConcurrentHashMap.java
+++ src/main/java/util/concurrent/ConcurrentHashMap.java
@@ -148,11 +148,11 @@ public class ConcurrentHashMap<K, V> extends AbstractMap<K, V>
     /**
      * Applies a supplemental hash function to a given hashCode, which
      * defends against poor quality hash functions.  This is critical
-     * because HashMap uses power-of two length hash tables, that
-     * otherwise encounter collisions for hashCodes that do not differ
-     * in lower bits. 
+     * because ConcurrentHashMap uses power-of-two length hash tables,
+     * that otherwise encounter collisions for hashCodes that do not
+     * differ in lower bits.
      */
-    static int hash(int h) {
+    private static int hash(int h) {
         // This function ensures that hashCodes that differ only by
         // constant multiples at each bit position have a bounded
         // number of collisions (approximately 8 at default load factor).

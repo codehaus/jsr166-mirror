76fd6ee75249ef3b862d62bd3c6a79bbcc1ef84d
jsr166 <jsr166>
2006-06-26 02:43:54 +0000

    coding standards

--- src/main/java/util/concurrent/ThreadFactory.java
+++ src/main/java/util/concurrent/ThreadFactory.java
@@ -30,12 +30,12 @@ package java.util.concurrent;
 public interface ThreadFactory {
 
     /**
-     * Constructs a new <tt>Thread</tt>.  Implementations may also initialize
-     * priority, name, daemon status, <tt>ThreadGroup</tt>, etc.
+     * Constructs a new {@code Thread}.  Implementations may also initialize
+     * priority, name, daemon status, {@code ThreadGroup}, etc.
      *
      * @param r a runnable to be executed by new thread instance
-     * @return constructed thread, or <tt>null</tt> if the request to 
-     * create a thread is rejected.
+     * @return constructed thread, or {@code null} if the request to 
+     *         create a thread is rejected
      */
     Thread newThread(Runnable r);
 }

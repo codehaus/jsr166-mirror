3351191436d223b7e4130e8ffadd50318d15bbef
jsr166 <jsr166>
2007-05-20 08:23:00 +0000

    legal notice

--- src/main/java/util/concurrent/TimeUnit.java
+++ src/main/java/util/concurrent/TimeUnit.java
@@ -1,8 +1,8 @@
 /*
-  * Written by Doug Lea with assistance from members of JCP JSR-166
-  * Expert Group and released to the public domain, as explained at
-  * http://creativecommons.org/licenses/publicdomain
-  */
+ * Written by Doug Lea with assistance from members of JCP JSR-166
+ * Expert Group and released to the public domain, as explained at
+ * http://creativecommons.org/licenses/publicdomain
+ */
 
 package java.util.concurrent;
 

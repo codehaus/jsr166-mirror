64fa1fcc560d4c29796d4424aca96756889cde0b
dl <dl>
2009-11-15 12:26:48 +0000

    improve estimate in getEstimatedSurplusTaskCount

--- src/main/java/util/concurrent/ForkJoinWorkerThread.java
+++ src/main/java/util/concurrent/ForkJoinWorkerThread.java
@@ -737,8 +737,65 @@ public class ForkJoinWorkerThread extends Thread {
      * function of number of idle workers.
      */
     final int getEstimatedSurplusTaskCount() {
-        // The halving approximates weighting idle vs non-idle workers
-        return (sp - base) - (pool.getIdleThreadCount() >>> 1);
+        /*
+         * The goal here is to provide a very cheap heuristic guide
+         * for task partitioning when programmers, frameworks, tools,
+         * or languages have little or no idea about task granularity.
+         * In essence by offering this method, we ask users only about
+         * tradeoffs in overhead vs expected throughput and its
+         * variance, rather than how finely to partition tasks.
+         *
+         * In a steady state strict (tree-structured) computation,
+         * each thread makes available for stealing enough tasks for
+         * other threads to remain active. Inductively, if all threads
+         * play by the same rules, each thread should make available
+         * only a constant number of tasks. 
+         *
+         * The minimum useful constant is just 1. But using a value of
+         * 1 would require immediate replenishment upon each steal to
+         * maintain enough tasks, which is infeasible.  Further,
+         * partitionings/granularities of offered tasks should
+         * minimize steal rates, which in general means that threads
+         * nearer the top of computation tree should generate more
+         * than those nearer the bottom. In perfect steady state, each
+         * thread is at approximately the same level of computation
+         * tree. However, producing extra tasks amortizes the
+         * uncertainty of progress and diffusion assumptions.
+         *
+         * So, users will want to use values larger, but not much
+         * larger than 1 to both smooth over transient shortages and
+         * hedge against uneven progress; as traded off against the
+         * cost of extra task overhead. We leave the user to pick a
+         * threshold value to compare with the results of this call to
+         * guide decisions, but recommend values such as 3. 
+         *
+         * When all threads are active, it is on average OK to
+         * estimate surplus strictly locally. In steady-state, if one
+         * thread is maintaining say 2 surplus tasks, then so are
+         * others. So we can just use estimated queue length (although
+         * note that (sp - base) can be an overestimate because of
+         * stealers lagging increments of base).
+         *
+         * However, this strategy alone leads to serious mis-estimates
+         * in some non-steady-state conditions (ramp-up, ramp-down,
+         * other stalls). We can detect many of these by further
+         * considering the number of "idle" threads, that are known to
+         * have zero queued tasks. A straight compensation would lead
+         * to weighting of the queued task estimate by a function of
+         * the proportion of idle threads.  However, we don't want to
+         * waste much calculation for the sake of weightings that only
+         * apply transiently, so cheapen this by (a) not bothering to
+         * weight at all unless there is more than one queued task (b)
+         * rather than compensating by a factor of (#idle/#active)
+         * threads, we just substract out a function of #idle that is
+         * a good enough approximation for conditions near the
+         * borderlines for threshold testing.  This errs in the
+         * direction of reporting more extreme lack of surplus (as in
+         * returning negative values) in cases where users should
+         * almost surely be generating tasks anyway.
+         */
+        int n = sp - base;
+        return n > 1? n - (pool.getIdleThreadCount() >>> 2) : n;
     }
 
     /**

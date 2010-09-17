89e24d56b5be30831f2358ba644bbffa297b2303
jsr166 <jsr166>
2010-09-17 00:53:15 +0000

    whitespace

--- src/test/tck/AbstractExecutorServiceTest.java
+++ src/test/tck/AbstractExecutorServiceTest.java
@@ -29,10 +29,15 @@ public class AbstractExecutorServiceTest extends JSR166TestCase {
     static class DirectExecutorService extends AbstractExecutorService {
         public void execute(Runnable r) { r.run(); }
         public void shutdown() { shutdown = true; }
-        public List<Runnable> shutdownNow() { shutdown = true; return Collections.EMPTY_LIST; }
+        public List<Runnable> shutdownNow() {
+            shutdown = true;
+            return Collections.EMPTY_LIST;
+        }
         public boolean isShutdown() { return shutdown; }
         public boolean isTerminated() { return isShutdown(); }
-        public boolean awaitTermination(long timeout, TimeUnit unit) { return isShutdown(); }
+        public boolean awaitTermination(long timeout, TimeUnit unit) {
+            return isShutdown();
+        }
         private volatile boolean shutdown = false;
     }
 

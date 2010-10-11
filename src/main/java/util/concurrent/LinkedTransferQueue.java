12be1d29040c596dfd127156f2cf58c1d1034a18
jsr166 <jsr166>
2010-10-11 18:27:39 +0000

    sync offer method @return javadoc

--- src/main/java/util/concurrent/LinkedTransferQueue.java
+++ src/main/java/util/concurrent/LinkedTransferQueue.java
@@ -969,8 +969,7 @@ public class LinkedTransferQueue<E> extends AbstractQueue<E>
      * Inserts the specified element at the tail of this queue.
      * As the queue is unbounded, this method will never return {@code false}.
      *
-     * @return {@code true} (as specified by
-     *         {@link BlockingQueue#offer(Object) BlockingQueue.offer})
+     * @return {@code true} (as specified by {@link Queue#offer})
      * @throws NullPointerException if the specified element is null
      */
     public boolean offer(E e) {

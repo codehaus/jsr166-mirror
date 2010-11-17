305479ce2e56ad8b919244442c6c87023b499ce3
dl <dl>
2010-11-17 10:49:53 +0000

    Sync with upstream @link fix

--- src/main/java/util/concurrent/ConcurrentLinkedQueue.java
+++ src/main/java/util/concurrent/ConcurrentLinkedQueue.java
@@ -36,8 +36,8 @@ import java.util.Queue;
  * <p>Iterators are <i>weakly consistent</i>, returning elements
  * reflecting the state of the queue at some point at or since the
  * creation of the iterator.  They do <em>not</em> throw {@link
- * ConcurrentModificationException}, and may proceed concurrently with
- * other operations.  Elements contained in the queue since the creation
+ * java.util.ConcurrentModificationException}, and may proceed concurrently
+ * with other operations.  Elements contained in the queue since the creation 
  * of the iterator will be returned exactly once.
  *
  * <p>Beware that, unlike in most collections, the {@code size} method

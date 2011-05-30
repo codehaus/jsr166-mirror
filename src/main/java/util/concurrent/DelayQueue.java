c2d376f47dcafa691c82904939d85def8606326f
dl <dl>
2011-05-30 23:08:12 +0000

    Clarify documented Iterator behavior

--- src/main/java/util/concurrent/DelayQueue.java
+++ src/main/java/util/concurrent/DelayQueue.java
@@ -25,7 +25,9 @@ import java.util.*;
  *
  * <p>This class and its iterator implement all of the
  * <em>optional</em> methods of the {@link Collection} and {@link
- * Iterator} interfaces.
+ * Iterator} interfaces.  The Iterator provided in method {@link
+ * #iterator()} is <em>not</em> guaranteed to traverse the elements of
+ * the DelayQueue in any particular order.
  *
  * <p>This class is a member of the
  * <a href="{@docRoot}/../technotes/guides/collections/index.html">

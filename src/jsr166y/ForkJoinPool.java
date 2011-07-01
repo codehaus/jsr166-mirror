b784b7aa4b3303692069d4a76b1830799e412285
jsr166 <jsr166>
2011-07-01 03:09:02 +0000

    tiny javadoc fixes

--- src/jsr166y/ForkJoinPool.java
+++ src/jsr166y/ForkJoinPool.java
@@ -545,8 +545,8 @@ public class ForkJoinPool extends AbstractExecutorService {
     volatile boolean shutdown;
 
     /**
-     * True if use local fifo, not default lifo, for local polling
-     * Read by, and replicated by ForkJoinWorkerThreads
+     * True if use local fifo, not default lifo, for local polling.
+     * Read by, and replicated by ForkJoinWorkerThreads.
      */
     final boolean locallyFifo;
 
@@ -1321,7 +1321,7 @@ public class ForkJoinPool extends AbstractExecutorService {
     // misc ForkJoinWorkerThread support
 
     /**
-     * Increment or decrement quiescerCount. Needed only to prevent
+     * Increments or decrements quiescerCount. Needed only to prevent
      * triggering shutdown if a worker is transiently inactive while
      * checking quiescence.
      *
@@ -1334,9 +1334,9 @@ public class ForkJoinPool extends AbstractExecutorService {
     }
 
     /**
-     * Directly increment or decrement active count without
-     * queuing. This method is used to transiently assert inactivation
-     * while checking quiescence.
+     * Directly increments or decrements active count without queuing.
+     * This method is used to transiently assert inactivation while
+     * checking quiescence.
      *
      * @param delta 1 for increment, -1 for decrement
      */

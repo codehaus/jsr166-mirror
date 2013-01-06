f432e9bb88fd2f96e6feb4d58703174d266db995
jsr166 <jsr166>
2013-01-06 17:47:58 +0000

    javadoc style

--- src/main/java/util/concurrent/atomic/DoubleAccumulator.java
+++ src/main/java/util/concurrent/atomic/DoubleAccumulator.java
@@ -148,7 +148,7 @@ public class DoubleAccumulator extends Striped64 implements Serializable {
     }
 
     /**
-     * Returns the String representation of the current value
+     * Returns the String representation of the current value.
      * @return the String representation of the current value
      */
     public String toString() {

7177573ca76a1bf2564d64f989cbca7767cf6a7b
jsr166 <jsr166>
2013-02-06 07:07:48 +0000

    improve obtrudeValue javadoc

--- src/jsr166e/CompletableFuture.java
+++ src/jsr166e/CompletableFuture.java
@@ -2640,10 +2640,10 @@ public class CompletableFuture<T> implements Future<T> {
 
     /**
      * Forcibly sets or resets the value subsequently returned by
-     * method get() and related methods, whether or not already
-     * completed. This method is designed for use only in error
-     * recovery actions, and even in such situations may result in
-     * ongoing dependent completions using established versus
+     * method {@link #get()} and related methods, whether or not
+     * already completed. This method is designed for use only in
+     * error recovery actions, and even in such situations may result
+     * in ongoing dependent completions using established versus
      * overwritten outcomes.
      *
      * @param value the completion value

e0e90df960855799494a0bc894240e2c02773606
jsr166 <jsr166>
2013-02-10 21:18:25 +0000

    add testAllOf_empty, testAnyOf_empty

--- src/test/tck/CompletableFutureTest.java
+++ src/test/tck/CompletableFutureTest.java
@@ -1,9 +1,8 @@
 /*
- * Written by Doug Lea with assistance from members of JCP JSR-166
- * Expert Group and released to the public domain, as explained at
+ * Written by Doug Lea and Martin Buchholz with assistance from
+ * members of JCP JSR-166 Expert Group and released to the public
+ * domain, as explained at
  * http://creativecommons.org/publicdomain/zero/1.0/
- * Other contributors include Andrew Wright, Jeffrey Hayes,
- * Pat Fisher, Mike Judd.
  */
 
 import junit.framework.*;
@@ -28,6 +27,39 @@ public class CompletableFutureTest extends JSR166TestCase {
         return new TestSuite(CompletableFutureTest.class);
     }
 
+    void checkIncomplete(CompletableFuture<?> f) {
+        assertFalse(f.isDone());
+        assertFalse(f.isCancelled());
+        assertTrue(f.toString().contains("[Not completed]"));
+        try {
+            assertNull(f.getNow(null));
+        } catch (Throwable fail) { threadUnexpectedException(fail); }
+        try {
+            f.get(0L, SECONDS);
+            shouldThrow();
+        }
+        catch (TimeoutException success) {}
+        catch (Throwable fail) { threadUnexpectedException(fail); }
+    }
+
+    void checkCompletedNormally(CompletableFuture<?> f, Object value) {
+        assertTrue(f.isDone());
+        assertFalse(f.isCancelled());
+        assertTrue(f.toString().contains("[Completed normally]"));
+        try {
+            assertSame(value, f.join());
+        } catch (Throwable fail) { threadUnexpectedException(fail); }
+        try {
+            assertSame(value, f.getNow(null));
+        } catch (Throwable fail) { threadUnexpectedException(fail); }
+        try {
+            assertSame(value, f.get());
+        } catch (Throwable fail) { threadUnexpectedException(fail); }
+        try {
+            assertSame(value, f.get(0L, SECONDS));
+        } catch (Throwable fail) { threadUnexpectedException(fail); }
+    }
+
     // XXXX Just a skeleton implementation for now.
     public void testTODO() {
         fail("Please add some real tests!");
@@ -45,5 +77,22 @@ public class CompletableFutureTest extends JSR166TestCase {
         f = new CompletableFuture<String>();
         f.completeExceptionally(new IndexOutOfBoundsException());
         assertTrue(f.toString().contains("[Completed exceptionally]"));
+    }
+
+    /**
+     * allOf(no component futures) returns a future completed normally
+     * with the value null
+     */
+    public void testAllOf_empty() throws Exception {
+        CompletableFuture<?> f = CompletableFuture.allOf();
+        checkCompletedNormally(f, null);
+    }
+
+    /**
+     * anyOf(no component futures) returns an incomplete future
+     */
+    public void testAnyOf_empty() throws Exception {
+        CompletableFuture<?> f = CompletableFuture.anyOf();
+        checkIncomplete(f);
     }
 }

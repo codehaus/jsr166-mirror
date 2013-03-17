c2b9a8596628ad35b51c8f6a2c11605090d4ddb8
jsr166 <jsr166>
2013-03-17 19:20:21 +0000

    add missing @since

--- src/main/java/util/concurrent/CompletableFuture.java
+++ src/main/java/util/concurrent/CompletableFuture.java
@@ -387,6 +387,8 @@ public class CompletableFuture<T> implements Future<T> {
      * A marker interface identifying asynchronous tasks produced by
      * {@code async} methods. This may be useful for monitoring,
      * debugging, and tracking asynchronous activities.
+     *
+     * @since 1.8
      */
     public static interface AsynchronousCompletionTask {
     }

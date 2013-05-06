304cccd6cb88c4850ea8d823b7edaf18edc9508e
jsr166 <jsr166>
2013-05-06 18:50:41 +0000

    whitespace

--- src/main/java/util/concurrent/CopyOnWriteArrayList.java
+++ src/main/java/util/concurrent/CopyOnWriteArrayList.java
@@ -917,14 +917,14 @@ public class CopyOnWriteArrayList<E>
     }
 
     public void sort(Comparator<? super E> c) {
-         final ReentrantLock lock = this.lock;
-         lock.lock();
-         try {
-             Object[] elements = getArray();
-             Object[] newElements = Arrays.copyOf(elements, elements.length);
-             @SuppressWarnings("unchecked") E[] es = (E[])newElements;
-             Arrays.sort(es, c);
-             setArray(newElements);
+        final ReentrantLock lock = this.lock;
+        lock.lock();
+        try {
+            Object[] elements = getArray();
+            Object[] newElements = Arrays.copyOf(elements, elements.length);
+            @SuppressWarnings("unchecked") E[] es = (E[])newElements;
+            Arrays.sort(es, c);
+            setArray(newElements);
         } finally {
             lock.unlock();
         }

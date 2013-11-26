378292c50196babd511895fb2f5022994a2f2334
dl <dl>
2013-11-26 18:36:16 +0000

    Fix bad conditional

--- src/main/java/util/concurrent/CompletableFuture.java
+++ src/main/java/util/concurrent/CompletableFuture.java
@@ -1975,7 +1975,7 @@ public class CompletableFuture<T> implements Future<T>, CompletionStage<T> {
             }
             if (dst == null)
                 dst = new CompletableFuture<U>();
-            if (e == null || ex != null)
+            if (ex != null)
                 dst.internalComplete(null, ex);
         }
         helpPostComplete();

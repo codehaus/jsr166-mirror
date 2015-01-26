9615b508f9bdaab0bf07733d121d49420a158e6f
jsr166 <jsr166>
2015-01-26 17:22:25 +0000

    unused imports

--- src/test/loops/LongAdderLoops.java
+++ src/test/loops/LongAdderLoops.java
@@ -4,7 +4,6 @@
  * http://creativecommons.org/publicdomain/zero/1.0/
  */
 
-import java.util.*;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;

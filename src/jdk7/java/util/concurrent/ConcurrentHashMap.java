7eb7552b8e14ac9a7ef39352fd9086d3fac6a70e
jsr166 <jsr166>
2015-03-24 22:30:53 +0000

    refactor calls to putFields

--- src/jdk7/java/util/concurrent/ConcurrentHashMap.java
+++ src/jdk7/java/util/concurrent/ConcurrentHashMap.java
@@ -1235,9 +1235,10 @@ public class ConcurrentHashMap<K,V> implements ConcurrentMap<K,V>, Serializable 
             new Segment<?,?>[DEFAULT_CONCURRENCY_LEVEL];
         for (int i = 0; i < segments.length; ++i)
             segments[i] = new Segment<K,V>(LOAD_FACTOR);
-        s.putFields().put("segments", segments);
-        s.putFields().put("segmentShift", segmentShift);
-        s.putFields().put("segmentMask", segmentMask);
+        java.io.ObjectOutputStream.PutField streamFields = s.putFields();
+        streamFields.put("segments", segments);
+        streamFields.put("segmentShift", segmentShift);
+        streamFields.put("segmentMask", segmentMask);
         s.writeFields();
 
         Node<K,V>[] t;
